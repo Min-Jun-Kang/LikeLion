d3cabe65a106ce465a2343f68c11d5dce9bbe8a8
Min-Jun-Kang <zhfeps123@naver.com>
2025-02-21 16:02:33 +0900

    study

---
+++ studyReadLine/studyReadLine/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////사용자 입력을 문자열로 받기
+            //Console.Write("이름을 입력하세요: ");
+            //string username = Console.ReadLine(); //사용자로 부터 입력 받기
+
+            //Console.WriteLine($"안녕하세요, {username}님!"); //""안에 변수를 한번에 표현하는 방법
+            //Console.WriteLine("안녕하세요, "+ username +"님!");
+
+            ////문자열을 정수로 변환
+            //Console.Write("나이를 입력하세요: ");
+            //string input = Console.ReadLine(); //사용자로부터 입력 받기
+            //int age = int.Parse(input); //문자열을 정수로 변환
+
+            //Console.WriteLine($"내년에는 {age + 1} 살이 되겠군요!"); //변환된 값 사용
+            //Console.WriteLine("내년에는 " + (age+1) + " 살이 되겠군요!");//문자열 + 정수, ()으로 안묶으면 정수 계산이 안되고 출력됨
+            //Console.WriteLine("내년에는 {0} 살이 되겠군요!",age+1); //{0} , 정수형 변수
+
+            ////실습 : 입력을 받아서 출력하기
+            //Console.Write("루인 스킬 피해를 입력하세요: ");
+            //float ruin = float.Parse(Console.ReadLine());
+            //Console.Write("카드 게이지 획득량을 입력하세요: ");
+            //float card = float.Parse(Console.ReadLine());
+            //Console.Write("각성기 피해를 입력하세요: ");
+            //float awa = float.Parse(Console.ReadLine());
+            //Console.Write("최대마나를 입력하세요: ");
+            //int MaxMana = int.Parse(Console.ReadLine());
+            //Console.Write("전투 중 마나 회복량을 입력하세요: ");
+            //int BM = int.Parse(Console.ReadLine());
+            //Console.Write("비전투 중 마나 회복량을 입력하세요: ");
+            //int NBM = int.Parse(Console.ReadLine());
+            //Console.Write("이동 속도를 입력하세요: ");
+            //float speed = float.Parse(Console.ReadLine());
+            //Console.Write("탈 것 속도를 입력하세요: ");
+            //float VS = float.Parse(Console.ReadLine());
+            //Console.Write("운반 속도를 입력하세요: ");
+            //float TS = float.Parse(Console.ReadLine());
+            //Console.Write("스킬 재사용 대기시간 감소를 입력하세요: ");
+            //float CD = float.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //Console.WriteLine("활동");
+            //Console.WriteLine();
+            //Console.WriteLine($"루인 스킬 피해량                   {ruin}%");
+            //Console.WriteLine($"카드 게이지 획득량                 {card}%");
+            //Console.WriteLine($"각성기 피해                        {awa}%");
+            //Console.WriteLine($"최대 마나                          {MaxMana}");
+            //Console.WriteLine($"전투 중 마나 회복량                {BM}");
+            //Console.WriteLine($"비전투 중 마나 회복량              {NBM}");
+            //Console.WriteLine($"이동 속도                          {speed}");
+            //Console.WriteLine($"탈 것 속도                         {VS}");
+            //Console.WriteLine($"운반 속도                          {TS}");
+            //Console.WriteLine($"스킬 재사용 대기시간 감소          {CD}");
+
+        }
+    }
+}
