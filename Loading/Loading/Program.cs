14b4326c66453727987cce2e07c0b117153fc5ca
Min-Jun-Kang <zhfeps123@naver.com>
2025-02-21 15:04:17 +0900

    study

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading; //Thread를 쓰기 위한 조건
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(2000); // 1000 = 1초
+            Console.Clear();
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(2000); // 1000 = 1초
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000); // 1000 = 1초
+            Console.Clear();
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(3000); // 1000 = 1초
+            Console.Clear();
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000); // 1000 = 1초
+            Console.Clear();
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(2000); // 1000 = 1초
+            Console.Clear();
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000); // 1000 = 1초
+            Console.Clear();
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000); // 1000 = 1초
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000); // 1000 = 1초
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000); // 1000 = 1초
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■■");
+            Console.Clear();
+            Console.WriteLine("텍스트 엘든링");
+
+
+
+        }
+    }
+}
