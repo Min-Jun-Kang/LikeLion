c9fbf14fcdaebc49a0d586a161f21f04d637a964
Min-Jun-Kang <zhfeps123@naver.com>
2025-02-21 17:00:34 +0900

    study

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //로딩바 시작화면
+            //게임 스토리1
+            Console.WriteLine("로딩 중... □□□□□□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("로딩 중... ■□□□□□□□□□");
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine("로딩 중... ■■□□□□□□□□");
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine("로딩 중... ■■■□□□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("로딩 중... ■■■■□□□□□□");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("로딩 중... ■■■■■□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("로딩 중... ■■■■■■□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("로딩 중... ■■■■■■■□□□");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("로딩 중... ■■■■■■■■□□");
+            Thread.Sleep(300);
+            Console.Clear();
+            Console.WriteLine("로딩 중... ■■■■■■■■■□");
+            Thread.Sleep(200);
+            Console.Clear();
+            Console.WriteLine("로딩 중... ■■■■■■■■■■");
+            Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine("로딩 완료!");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("마족과의 전쟁이 일어난 지 10년 째...");
+            Console.ReadLine();
+            Console.WriteLine("단 하나의 성만을 제외하고 모든 곳은 함락되었습니다.");
+            Console.ReadLine();
+            Console.WriteLine("당신은 이 마지막 성을 지킬 성주 입니다.");
+            Console.ReadLine();
+            Console.WriteLine("세상을 구원 해주세요.");
+            Console.ReadLine();
+            Console.WriteLine("엔터 키를 눌러 게임을 시작해주세요!");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("봐 주셔서 감사합니다.");
+
+
+        }
+    }
+}
