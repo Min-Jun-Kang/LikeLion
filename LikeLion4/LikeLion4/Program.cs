ec7c361f868d3af564107e55aaa966f5ec7942f4
Min-Jun-Kang <zhfeps123@naver.com>
2025-02-21 13:13:37 +0900

    study

---
+++ LikeLion4/LikeLion4/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //주석 처리 : ctrl + k + c, 주석 해제 : ctrl + k + u
+            //주석 처리 : crtrl + shift + /
+            ////문자열
+            //string greeting; //문자열 변수를 선언
+            //greeting = "Hello World"; //변수에 값을 저장
+
+            ////변수의 값을 사용
+            //Console.WriteLine(greeting); //출력 : Hello World
+
+            //변수 선언과 초기화를 한번에 수행
+            //int score = 100; //정수형 변수 선언과 동시에 100으로 초기화
+            //double temperature = 36.5; //실수형 변수 선언과 초기화
+            //string city = "Seoul"; //문자열 변수 선언과 초기화
+
+            ////변수 출력
+            //Console.WriteLine(score); //출력 100
+            //Console.WriteLine(temperature); // 출력 36.5
+            //Console.WriteLine(city); // 출력 Seoul
+
+            ////같은 데이터 타입은 한 번에 선언 초기화 가능
+            //int x = 10, y = 20, z = 30;
+
+            //Console.WriteLine(x + "," + y + "," + z);
+
+            ////상수 사용하기
+            ////상수 : 프로그램 내에서 변하지 않는 중요한 값을 정의할 때 사용한다.
+            ////상수는 "고정된 값"으로 중요한 값이 실수로 변경되는 것을 방지한다.
+
+            //const double pi = 3.14159; //상수 pi 선언 및 초기화
+            //const int MaxScore = 100; //정수형 상수 선언 및 초기화
+
+            //pi = 3.1578; //에러가 납니다.
+            //MaxScore = 102; //에러가 납니다.
+
+            ////출력
+            //Console.WriteLine("Pi : " + pi);
+            //Console.WriteLine("MaxScore : " + MaxScore);
+
+            //// 캐릭터의 스탯 출력
+
+            //int att = 16755;
+            //int hp = 78103;
+            //int critical = 36;
+            //int specializaiton= 1017;
+            //int overpower = 41;
+            //int quick = 611;
+            //int patience= 22;
+            //int skilled = 39;
+
+            //Console.WriteLine("기본 특성");
+            //Console.WriteLine();
+            //Console.WriteLine("공격력 : " + att);
+            //Console.WriteLine("최대 생명력 : " + hp);
+            //Console.WriteLine();
+            //Console.WriteLine("전투 특성");
+            //Console.WriteLine();
+            //Console.WriteLine("치명 : " + critical);
+            //Console.WriteLine("특화 : " + specializaiton);
+            //Console.WriteLine("제압 : " + overpower);
+            //Console.WriteLine("신속 : " + quick);
+            //Console.WriteLine("인내 : " + patience);
+            //Console.WriteLine("숙련 : " + skilled);
+
+        }
+    }
+}
