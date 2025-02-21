372282b140d3510d3154523f5bafacbb2707d1e0
Min-Jun-Kang <zhfeps123@naver.com>
2025-02-21 10:22:55 +0900

    study int

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수 선언 : 데이터 타입과 변수 이름을 저장합니다.
+            int age; //정수형 변수
+            age = 25; //변수에 값 저장
+
+            Console.WriteLine(25); //출력하고 개행(enter 기능)
+            Console.Write(25); //변수에 저장된 값을 출력
+            
+        }
+    }
+}
