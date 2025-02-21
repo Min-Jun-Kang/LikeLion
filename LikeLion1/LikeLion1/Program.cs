083303c11ebfca8fba6566ac056d8a38d1dfe74a
Min-Jun-Kang <zhfeps123@naver.com>
2025-02-21 09:57:19 +0900

    study

---
+++ LikeLion1/LikeLion1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //출력 (주석 처리)
+            Console.WriteLine("깃허브 연결하기");
+        }
+    }
+}
