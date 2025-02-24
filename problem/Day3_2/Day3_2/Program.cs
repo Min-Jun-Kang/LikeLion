d00d47e2c13f478f7e701a3ef1aa50e98cd93d9d
Min-Jun-Kang <zhfeps123@naver.com>
2025-02-24 16:40:09 +0900

    study

--- problem/Day3_2/Day3_2/Program.cs
+++ problem/Day3_2/Day3_2/Program.cs
@@ -10,7 +10,129 @@ namespace Day3_2
     {
         static void Main(string[] args)
         {
-            
+            //2024오후문제
+            //문제 1.세 정수의 최대값 구하기
+            //문제 설명:
+            //사용자로부터 3개의 정수를 입력받아 그 중 가장 큰 값을 출력하는 프로그램을 작성하세요.
+            //요구사항:
+
+            ////사용자에게 세 개의 정수를 입력받습니다.
+            //int num1 = 0, num2 = 0, num3 = 0;
+            //int maxNum = 0;
+            //Console.Write("첫 번째 수를 입력하세요 : ");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.Write("두 번째 수를 입력하세요 : ");
+            //num2 = int.Parse(Console.ReadLine());
+            //Console.Write("세 번째 수를 입력하세요 : ");
+            //num3 = int.Parse(Console.ReadLine());
+            ////if문을 사용하여 가장 큰 정수를 결정합니다.
+            //if (num1 > num2)
+            //{
+            //    if (num1 > num3)
+            //    {
+            //        maxNum = num1;
+            //    }
+            //    else
+            //    {
+            //        maxNum = num3;
+            //    }
+            //}
+            //else
+            //{
+            //    if (num2 > num3)
+            //    {
+            //        maxNum = num2;
+            //    }
+            //    else
+            //    {
+            //        maxNum = num3;
+            //    }
+            //}
+            ////결과를 “최대값: X” 형식으로 출력합니다.
+            //Console.WriteLine($"최대값 : {maxNum}");
+
+            //문제 2.점수에 따른 학점 평가
+            //문제 설명:
+
+            //학생의 점수를 입력받아 아래 기준에 따라 학점을 출력하는 프로그램을 작성하세요.
+            //Console.Write("점수를 입력해주세요 : ");
+            //int score = int.Parse(Console.ReadLine());
+            ////90 이상: A 학점
+            //if (score >= 90)
+            //{
+            //    Console.WriteLine("A 학점 입니다.");
+            //}
+            ////80 이상 90 미만: B 학점
+            //else if (score < 90 && score >= 80) {
+            //    Console.WriteLine("B 학점 입니다.");
+            //}
+            ////70 이상 80 미만: C 학점
+            //else if (score < 80 && score >= 70)
+            //{
+            //    Console.WriteLine("C 학점 입니다.");
+            //}
+            ////60 이상 70 미만: D 학점
+            //else if (score < 70 && score >= 60)
+            //{
+            //    Console.WriteLine("D 학점 입니다.");
+            //}
+            ////60 미만: F 학점
+            //else {
+            //    Console.WriteLine("F 학점 입니다.");
+            //}
+
+            ////문제 3.간단한 사칙연산 계산기
+            ////문제 설명:
+            ////사용자로부터 두 개의 숫자와 연산자(+, -, *, /)를 입력받아 해당 연산을 수행하고 결과를 출력하는 계산기 프로그램을 작성하세요.
+            ////요구사항:
+
+            ////두 개의 숫자와 연산자 기호를 입력받습니다.
+            //int num1 = 0, num2 = 0;
+            //string sym = null;
+            //int value = 0;
+            //Console.Write("첫 번째 수를 입력해주세요: ");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.Write("연산자를 입력해주세요(+, -, *, /) : ");
+            //sym = Console.ReadLine();
+            //Console.Write("두 번째 수를 입력해주세요: ");
+            //num2 = int.Parse(Console.ReadLine());
+            ////if문을 사용하여 연산자를 확인하고 해당 연산을 수행합니다.
+            //if (sym == "+" || sym == "-" || sym == "*" || sym == "/")
+            //{
+            //    if (sym == "+")
+            //    {
+            //        value = num1 + num2;
+            //    }
+            //    else if (sym == "-")
+            //    {
+            //        value = num1 - num2;
+            //    }
+            //    else if (sym == "*")
+            //    {
+            //        value = num1 * num2;
+            //    }
+            //    else 
+            //    {
+            //        //나눗셈의 경우 0으로 나누는 상황을 처리하여 에러 메시지를 출력합니다.
+            //        if (num2 == 0) 
+            //        {
+            //            Console.WriteLine("0으로 나눌 수는 없습니다.");
+            //        }
+            //        value = num1 / num2;
+
+            //    }
+            //    //결과는 “결과: X” 형식으로 출력합니다.
+            //    Console.WriteLine($"계산 결과는 {value}입니다.");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("연산자를 다시 입력해주세요.");
+            //}
+
+
+
+            ////시간남는사람
+            //나만의 텍스트노벨 배운내용 더 넣어서 만들기
 
         }
     }
