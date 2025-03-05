d9ecb06cb5b96711d598488129d7a8558e13161e
Min-Jun-Kang <zhfeps123@naver.com>
2025-03-05 14:20:46 +0900

    study

---
+++ 수업 내용/Class/Day9_3/Day9_3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_3
+{
+    class Player
+    {
+        public string name;
+
+        public void Render()
+        {
+            Console.WriteLine("플레이어" + name);
+        }
+    }
+
+    class Wizard : Player
+    {
+        public string job;
+        public void Render2()
+        {
+            Console.WriteLine("직업은 " + job + "입니다.");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p = new Player();  //객체
+
+            p.name = "홍길동";
+            p.Render();
+            //p.Render2(); //접근 불가
+
+
+            Wizard w = new Wizard(); //얘는 player에도 접근가능
+            w.name = "대마법사 샤우론";
+            w.job = "마법사";
+
+            w.Render();
+            w.Render2();
+        }
+    }
+}
---
+++ 수업 내용/Class/Day9_4/Day9_4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_4
+{
+    class Animal
+    {
+        public virtual void speak()
+        {
+            Console.WriteLine("동물이 소리를 냅니다.");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void speak()
+        {
+            Console.WriteLine("멍멍!");
+        }
+        public void WagTail()
+        {
+            Console.WriteLine("꼬리를 흔든다.");
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Animal ani = new Animal();
+            Dog dog = new Dog();
+            ani = (Animal)dog;
+            dog = (Dog)ani; //형 변환 개념으로 이해하면 쉽다.
+
+            double fl = 2.0;
+            int nol = 2;
+
+            fl = nol;
+            nol = (int)fl;
+
+            Animal myAnimal = new Dog(); //업캐스팅
+
+            myAnimal.speak(); //오버라이딩
+
+            Dog myDog = (Dog)myAnimal; //다운캐스팅
+
+            myDog.WagTail();
+
+            Animal myAni = new Animal();
+
+            myAni.speak();
+            
+
+        }
+    }
+}
